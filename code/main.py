ce87fac0cd8967b971b3d91ef9f0869266fd03ae
unknown <thomleysens@gmail.com>
2018-06-21 17:57:56 +0200

    Add code to automate the creation of PNG output using a jsonfile as parameter. See explanations in a futur update of README. WIP

--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-
+@author: thomas
 """
 from datetime import date
 import os
---
+++ code/params/csv_to_json.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 21 13:41:28 2018
+
+@author: thomas
+"""
+
+import pandas as pd
+from ast import literal_eval
+
+def csv_to_json(csv_file, json_file, sep, columns_with_array):
+    df = pd.read_csv(csv_file, sep=sep)
+    
+    for column in columns_with_array:
+        df[column] = df[column].apply(literal_eval)
+    
+    df.to_json(json_file, orient='records')
+
+if __name__ == "__main__":
+    csv_file = "params_auto.tsv"
+    json_file = "params_auto.json"
+    sep = "\t"
+    columns_with_array = ["colors_iso","adresses"]
+    
+    df = csv_to_json(csv_file, json_file, sep, columns_with_array)
