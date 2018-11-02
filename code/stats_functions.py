05ed31cc23b3a24d38861b94ff0cdd5b16cf4b04
unknown <thomleysens@gmail.com>
2018-11-02 18:10:21 +0100

    Improve middle multipolygon calculations and update notebook

--- code/stats_functions.py
+++ code/stats_functions.py
@@ -13,11 +13,16 @@ import pyproj
     
 
 class GetMiddleMultiPoly:
-    def __init__(self, multis):
+    def __init__(self, multis, ratio_diff):
         """
-        
+        @multis (list): list of Polygons or MultiPolygons
+        @ratio_diff (float): Minimum ratio for "middle" calculation between
+        all "secondary" polygons. If intersection(poly_A, poly_B).area/poly_A.area
+        is inferior to or equal to this ratio, "middle" calculation is considered 
+        possible. See self.get_diff_area
         """
         self.multis = multis
+        self.ratio_diff = ratio_diff
         
     def iterate_for_middle(self):
         """
@@ -104,7 +109,7 @@ class GetMiddleMultiPoly:
                                 )
                         list_interiors.append(coords)
         
-        return Polygon(shell=new_outer_ring, holes=list_interiors)
+        return Polygon(shell=new_outer_ring, holes=list_interiors).buffer(0) #add buffer(0) to avoid invalid geometry
     
     def get_major_poly(self, multi_poly):
         """
@@ -120,7 +125,7 @@ class GetMiddleMultiPoly:
                 max_area = poly.area
                 major_poly = poly
         
-        return major_poly
+        return major_poly.buffer(0) #add buffer(0) to avoid invalid geometry
                       
     
     def reproject(self, element, inproj, outproj):
@@ -169,44 +174,54 @@ class GetMiddleMultiPoly:
         multi_contains = self.get_multi_poly(spatial_object_2)
         self.multi_contains = self.delete_inner_holes(multi_contains)
         
-        polys = []
+        self.polys = []
         
         #Get the majors poly and manage them first
         if len(list(self.multi_within)) > 1:
-            major_within = self.get_major_poly(self.multi_within)
+            self.major_within = self.get_major_poly(self.multi_within)
         else:
-            major_within = self.multi_within[0]
+            self.major_within = self.multi_within[0]
             
         if len(list(self.multi_contains)) > 1:
-            major_contains = self.get_major_poly(self.multi_contains)
+            self.major_contains = self.get_major_poly(self.multi_contains) 
         else:
-            major_contains = self.multi_contains[0]
+            self.major_contains = self.multi_contains[0]
         
         
         if (
-                major_within.within(major_contains) is True
+                self.major_within.within(self.major_contains) is True
                 ) or (
-                        self.get_offset(major_within).within(major_contains) is True
+                        self.get_offset(self.major_within).within(self.major_contains) is True
                         ) :
             
-            new_major_poly = self.middle_poly(major_within, major_contains)
-            polys.append(new_major_poly)
-            
-            for poly_within in self.multi_within:
-                for poly_contains in self.multi_contains:
-                    if poly_within.within(poly_contains) is True and (poly_contains != major_contains and poly_within != major_within):
-                        polys.append(self.middle_poly(poly_within, poly_contains))
+            new_major_poly = self.middle_poly(self.major_within, self.major_contains)
+            self.polys.append(new_major_poly)
         
-            middle_multi = MultiPolygon(polys)
+            middle_multi = self.middle_secondary()
         
         else:
-            if major_contains.intersects(major_within) is True:
-                polys.append(self.middle_poly(major_within, major_contains))
-                middle_multi = MultiPolygon(polys)
+            if self.major_contains.intersects(self.major_within) is True:
+                self.polys.append(self.middle_poly(self.major_within, self.major_contains))
+                middle_multi = self.middle_secondary()
             else:
                 middle_multi = MultiPolygon([])
                 raise ValueError("No intersections, middle multipolygon can't be measured, returns empty MultiPolygon")
         
+        return middle_multi
+    
+    def middle_secondary(self):
+        for poly_within in self.multi_within:
+            for poly_contains in self.multi_contains:
+                if (
+                        poly_within.within(poly_contains) is True 
+                        or self.get_diff_area(poly_within, poly_contains) is True
+                        )  and (
+                                poly_contains != self.major_contains and poly_within != self.major_within
+                                ):
+                    self.polys.append(self.middle_poly(poly_within, poly_contains))
+        
+            middle_multi = MultiPolygon(self.polys)
+            
         return middle_multi
         
     
@@ -226,3 +241,21 @@ class GetMiddleMultiPoly:
             new_poly = Polygon(new_linear)
             
         return new_poly
+    
+    def get_diff_area(self, poly_A, poly_B):
+        """
+        Returns a boolean
+        If difference between 2 polygons is inferior or equal to ratio, we 
+        consider that we can make a "middle" polygon from them
+        """
+        middle_possible = False
+        
+        if poly_A.is_valid is False:
+            poly_A = poly_A.buffer(0)
+        elif poly_B.is_valid is False:
+            poly_B = poly_B.buffer(0)
+        else:
+            if poly_A.difference(poly_B).area <= int(poly_A.area*self.ratio_diff):
+                middle_possible = True
+            
+        return middle_possible
